be60546ac2b1c4f8a6ecb96e50fbf2d734b9b13d
luislasn <luis.lasn@gmail.com>
2022-02-15 15:45:15 -0300

    Código-fonte da aula

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -74,7 +74,7 @@ public class RestauranteController {
 			
 			if (restauranteAtual.isPresent()) {
 				BeanUtils.copyProperties(restaurante, restauranteAtual.get(), 
-						"id", "formasPagamento");
+						"id", "formasPagamento", "endereco");
 				
 				Restaurante restauranteSalvo = cadastroRestaurante.salvar(restauranteAtual.get());				
 				return ResponseEntity.ok(restauranteSalvo);
