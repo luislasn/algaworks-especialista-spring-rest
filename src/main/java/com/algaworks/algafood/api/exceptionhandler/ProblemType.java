83f3999d1f6d13dc1ddddd06399846b15eae0aa3
luislasn <luis.lasn@gmail.com>
2022-02-25 23:22:21 -0300

    Código-fonte da aula

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
-	MENSAGEM_IMCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem Imcompreensível"),
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem Imcompreensível"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");  
