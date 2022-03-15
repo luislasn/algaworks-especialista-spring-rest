1976117e747b44dc998a2106fa1f967fa49dc0a6
luislasn <luis.lasn@gmail.com>
2022-03-14 22:50:39 -0300

    Código-fonte da aula

--- src/main/java/com/algaworks/algafood/jpa/formapagamento/BuscaFormaPagamentoMain.java
+++ src/main/java/com/algaworks/algafood/jpa/formapagamento/BuscaFormaPagamentoMain.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.jpa.formapagamento;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.domain.exception.FormaPagamentoNaoEncontradoException;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.AlgafoodApiApplication;
@@ -16,7 +18,8 @@ public class BuscaFormaPagamentoMain {
 		
 		FormaPagamentoRepository formaPagamentoRepository = applicationContext.getBean(FormaPagamentoRepository.class);
 		
-		FormaPagamento formaPagamento = formaPagamentoRepository.buscar(1L);
+		FormaPagamento formaPagamento = formaPagamentoRepository.findById(1L)
+				.orElseThrow(() -> new FormaPagamentoNaoEncontradoException(1L));
 		
 		System.out.println(formaPagamento.getDescricao());
 	}
