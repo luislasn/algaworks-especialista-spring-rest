adbdd8eb1b07edc358e753b64c3802b8de4bdc9f
luislasn <luis.lasn@gmail.com>
2022-03-17 23:23:38 -0300

    Código-fonte da aula

---
+++ src/main/java/com/algaworks/algafood/domain/exception/PermissaoNaoEncontradoException.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.domain.exception;
+
+public class PermissaoNaoEncontradoException extends EntidadeNaoEncontradaException {
+
+	private static final long serialVersionUID = 1L;	
+	
+	public PermissaoNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}	
+	
+	public PermissaoNaoEncontradoException(Long permissaoId) {
+		this(String.format("Não existe um cadastro de permissao com código %d", permissaoId));
+	}
+
+}
