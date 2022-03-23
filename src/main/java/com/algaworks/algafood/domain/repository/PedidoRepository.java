c0d19fa37b7a532d4550d73375764df1727db30b
luislasn <luis.lasn@gmail.com>
2022-03-22 23:38:48 -0300

    Código-fonte da aula

--- src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
@@ -1,12 +1,15 @@
 package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 
 import com.algaworks.algafood.domain.model.Pedido;
 
 public interface PedidoRepository extends CustomJpaRepository<Pedido, Long> {
+	
+	Optional<Pedido> findByCodigo(String codigo);
 	
 	@Query("from Pedido p join fetch p.cliente join fetch p.restaurante r join fetch r.cozinha")
 	List<Pedido> findAll();
